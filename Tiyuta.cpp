9e2ddfbec5035b25406b5fe0b069756dc6182b2e
talsha1995 <tal5shamir@gmail.com>
2021-01-23 13:57:27 +0200

    test append for char2

--- Tiyuta.cpp
+++ Tiyuta.cpp
@@ -26,7 +26,7 @@ public:
         counter++;
     }
 
-    Store(char *name) : file_name((name)), size(sizeof(T)), fileSize(0), file() {
+    Store(const char *name) : file_name((name)), size(sizeof(T)), fileSize(0), file() {
 
     }
 
@@ -68,41 +68,42 @@ private:
     unsigned int size; //size of object
     unsigned int fileSize; //number of objects in storage
     fstream file;
-    static int counter;
+//    static int counter;
 
 
 public:
 
-    Store() : file_name(("temp")), size(sizeof(char)), fileSize(0), file() {
+    Store() : file_name(("temp")), size(sizeof(char)), fileSize(0), file(file_name, ios::out | ios::binary) {
         //file.open(file_name);
-        counter++;
+//        counter++;
     }
 
-    Store(char *name) : file_name((name)), size(sizeof(char)), fileSize(0), file() {
+    Store(char *name) : file_name((name)), size(sizeof(char)), fileSize(0),  file() {
 
     }
 
 
-    void append(const char &c) {
+    void append(char &c) {
         //TODO file.write(reinterpret_cast<const char*>(&obj)
-        file.open(file_name,  ios::binary);
-        file.write(&c, size);
+        file.open(file_name, ios::app | ios::binary);
+        file.seekg(fileSize * size);
+        file.write(reinterpret_cast<const char*>(&c), size);
         file.close();
         fileSize++;
 
     }
 
     void write(const char &c, int i) {
-        file.open(file_name, ios::app | ios::binary);
+        file.open(file_name, ios::out | ios::binary);
         //TODO check counter range and what happened to file
         file.seekg(i * size);
-        file.write(&c, size);
+        file.write(reinterpret_cast<const char*>(&c),size);
         file.close();
     }
 
     char &read(int i) { // TODO check index bounds
         char buff;
-        file.open(file_name, ios::in);
+        file.open(file_name, ios::in | ios::binary);
         file.seekg(i * size);
         file.read(&buff, size);
         file.close();
@@ -112,9 +113,15 @@ public:
 };
 
 int main() {
-    char c='f', b='a';
-    Store<char> my_s("Test");
-    my_s.append(c);
+    char c ='a', b='f', e = 'r' , r= 'd' ;
 
+    Store<char> my_s("Test.bin");
+    my_s.append(c);
+    my_s.append(b);
+    my_s.append(e);
+    my_s.append(r);
+//    my_s.append(*(c+4));
+//    g = my_s.read(0);
+    cout << my_s.read(3);
     return 0;
 }
