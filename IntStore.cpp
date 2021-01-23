17c788f7a533a63b966d652a8eb929fe9370b7b9
Michael Shevelin <shevel55505@gmail.com>
2021-01-23 16:21:59 +0200

    int specialization added and tested

---
+++ IntStore.cpp
@@ -0,0 +1,5 @@
+//
+// Created by misha on 23/01/2021.
+//
+
+#include "IntStore.h"
--- Tiyuta.cpp
+++ Tiyuta.cpp
@@ -7,6 +7,7 @@
 #include <vector>
 #include <cstring>
 #include <sstream>
+#include <tr1/cstdint>
 
 using namespace std;
 int i = 0;
@@ -136,19 +137,83 @@ public:
 
 };
 
+
+
+template<>
+class Store<int> {
+private:
+    const char *file_name;
+    unsigned int size; //size of object
+    unsigned int fileSize; //number of objects in storage
+    fstream file;
+    static int counter;
+
+
+public:
+
+    Store() : file_name(("temp")), size(sizeof(int)), fileSize(0), file() {
+        counter++;
+    }
+    Store(char *name) : file_name((name)), size(sizeof(int)), fileSize(0),  file() {
+
+    }
+
+
+    void append(int &n) {
+        file.open(file_name, ios::app | ios::binary);
+        if (!file){
+            stringstream ss1, ss2(file_name);
+            ss1 << "Could not open the file ";
+            ss1 << ss2.str() << "\n";
+            throw runtime_error( ss1.str());
+        }
+        file.write(reinterpret_cast<const char*>(&n), size);
+        file.close();
+        fileSize++;
+    }
+
+    void write(const int &n, int i) {
+        file.open(file_name, ios::binary | ios::out | ios::in);
+        if (!file){
+            stringstream ss1, ss2(file_name);
+            ss1 << "Could not open the file ";
+            ss1 << ss2.str() << "\n";
+            throw runtime_error( ss1.str());
+        }
+        if (i < 0 || i>= fileSize) throw range_error("index is out of bounds\n");
+        file.seekp(i * size, ios::beg);
+        file.write(reinterpret_cast<const char*>(&n),size);
+        file.close();
+    }
+
+    int &read(int i) { // TODO check index bounds
+        int *buff = new int(1);
+        file.open(file_name, ios::in | ios::binary);
+        if (!file){
+            stringstream ss1, ss2(file_name);
+            ss1 << "Could not open the file ";
+            ss1 << ss2.str() << "\n";
+            throw runtime_error( ss1.str());
+        }
+        if (i < 0 || i>= fileSize) throw range_error("index is out of bounds\n");
+        file.seekg(i * size);
+        file.read((char*)buff, size);
+        file.close();
+        return *buff;
+    }
+
+};
 int main() {
     char c ='a', b='f', e = 'r' , r= 'd' ;
 
-    Store<char> my_s("Test.bin");
-    string msg = "Could not open file ";
-    string file_name = "Trst.bin";
-//    my_s.append(c);
-//    my_s.append(b);
-//    my_s.append(e);
-//    my_s.append(r);
-//    my_s.write(b, 4);
-//    my_s.append(*(c+4));
-//    g = my_s.read(0);
-//    cout << my_s.read(4);
+    Store<int> s("TestInt.bin");
+    for (int k = 0; k < 10; k++) {
+        s.append(k);
+    }
+    s.write(3, 10);
+    for (int k = 0; k < 10; k++) {
+        int m = s.read(k);
+        cout << m << " ";
+    }
     return 0;
 }
